cd51c760a106ace61d3c139d00952127aeb03735
Suresh Kumar <99480758+rex-suresh@users.noreply.github.com>
2022-05-16 11:58:36 +0530

    Renamed createPage to createGame

--- fireFly/gameResources/createGame.js
+++ fireFly/gameResources/createGame.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+
 const createAttr = ([attribute, value]) => attribute + '=' + '"' + value + '"';
 const attributesOf = (attributes) => attributes.map(createAttr).join(' ');
 
@@ -10,35 +11,45 @@ const tag = (tag, contents, attValues = []) => {
 
 const div = (contents, attValues) => tag('div', contents, attValues);
 
-const blockOf = function (currentPos) {
+const divBlock = function (currentPos) {
   const { flyPos, foodPos } = this;
+  const tiles = {
+    fly: div(div('', [['class', 'fly']]), []),
+    food: div(div('', [['class', 'food']]), []),
+    tile: div('', []),
+    nest: div(div('', [['class', 'food nest']]), [])
+  };  
   
-  let content = '';
+  let divLine = tiles.tile;
   if (currentPos === flyPos) {
-    content = div('', [['class', 'fly']]);
-  } else if (currentPos === foodPos) {
-    content = div('', [['class', 'food']]);
+    divLine = tiles.fly;
   }
-  if (flyPos === foodPos) {
-    content = div('', [['class', 'food nest']]);
+  if (currentPos === foodPos) {
+    divLine = tiles.food;
+  }
+  if (flyPos === foodPos && currentPos === flyPos) {
+    divLine = tiles.nest;
   }
 
-  return div(content, []);
+  return divLine;
 };
 
-const generateDivs = function (tileCount, positions) {
-  const divBlock = blockOf.bind(positions);
+const generateDivBlock = function (tileCount, positions) {
+  const gameTiles = divBlock.bind(positions);
+
   return Array(tileCount).fill(1).map((one, index) =>
-    divBlock(one + index)).join('');
+    gameTiles(one + index)).join('');
 };
 
 const generatePage = function (template, game) {
   const { tileCount, positions } = game;
   const page = template.replace(/__board__/,
-    generateDivs(tileCount, positions));
+    generateDivBlock(tileCount, positions));
+
   fs.writeFileSync('index.html', page, 'utf8');
   fs.writeFileSync('./gameResources/flyMoves.json',
-    JSON.stringify(game), 'utf8');
+    JSON.stringify(game, null, 2), 'utf8');
+
   return game;
 };
 
