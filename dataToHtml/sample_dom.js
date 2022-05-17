57abd50e994884489aa3cc9e063ad1a5a392d57d
Suresh Kumar <99480758+rex-suresh@users.noreply.github.com>
2022-05-17 14:05:12 +0530

    Added sample_DOM for data to html coverter

---
+++ dataToHtml/sample_dom.js
@@ -0,0 +1,13 @@
+['html', {},
+  [
+    ['head', {},
+      ['title', {}, 'Sample Dom'],
+      ['link', { href: './style.css', rel: 'stylesheet' }]
+    ],
+    ['body', {},
+      ['div', { class: 'page-wrapper' },
+        ['div', { class: 'box' }, '']
+      ]
+    ]
+  ]
+];
--- scaler/scale.js
+++ scaler/scale.js
@@ -7,14 +7,15 @@ const scale = function (fromDomain, toDomain, value) {
   return toDomainPercentOfValue + toDomain.min;
 };
 
-const initialRange = {
-  min: 0,
-  max: 100
-};
+// const initialRange = {
+//   min: 0,
+//   max: 100
+// };
 
-const finalRange = {
-  min: 0,
-  max: 100
-};
+// const finalRange = {
+//   min: 0,
+//   max: 10
+// };
 
-scale(initialRange, finalRange, 20);
+// scale(initialRange, finalRange, 20);
+exports.scale = scale;
