740ced492c7c03d4549b305797465f73b6772d1c
Suresh Kumar <99480758+rex-suresh@users.noreply.github.com>
2022-05-17 10:28:01 +0530

    Added scale.js to scale a value from one range to another

---
+++ scaler/scale.js
@@ -0,0 +1,20 @@
+const scale = function (fromDomain, toDomain, value) {
+  const fromRange = fromDomain.max - fromDomain.min;
+  const toRange = toDomain.max - toDomain.min;
+  const fromDomainPercentOfValue = (value - fromDomain.min) / fromRange;
+
+  const toDomainPercentOfValue = fromDomainPercentOfValue * toRange;
+  return toDomainPercentOfValue + toDomain.min;
+};
+
+const initialRange = {
+  min: 0,
+  max: 100
+};
+
+const finalRange = {
+  min: 0,
+  max: 100
+};
+
+scale(initialRange, finalRange, 20);
